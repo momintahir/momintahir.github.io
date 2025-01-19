46203f39d10007b8959de7d2c29665f8bf147157
Momin Tahir <momintahir7@gmail.com>
2025-01-19 18:54:31 +0500

    working on footer content

--- index.js
+++ index.js
@@ -321,7 +321,6 @@ function populateExp_Edu(items, id) {
     for (let j = 0; j < items[i].tags.length; j++) {
       let spanTags = document.createElement("span");
       spanTags.className = "badge";
-        spanTags.style = "color: #ffffff;"
       spanTags.innerHTML = items[i].tags[j];
       divTags.append(spanTags);
     }
